dc8a0017c1d222309bd59bed078781ac94a3945d
Naveen-manokaran <naveen.manokaran@bootlabstech.com>
2022-09-20 10:43:50 +0530

    recheck module

--- main.tf
+++ main.tf
@@ -2,12 +2,12 @@ resource "azurerm_app_service_plan" "appservice" {
   name                = var.appservice_name
   location            = var.location
   resource_group_name = var.resource_group_name
-  kind                = "Linux"
-  reserved            = true
+  kind                = var.kind
+  reserved            = var.reserved
 
   sku {
-    tier = "Standard"
-    size = "S1"
+    tier = var.sku_tier
+    size = var.sku_size
   }
 }
 
@@ -18,6 +18,6 @@ resource "azurerm_linux_web_app" "web_app" {
   service_plan_id     = azurerm_app_service_plan.appservice.id
 
   site_config {
-    minimum_tls_version = "1.2"
+    minimum_tls_version = var.minimum_tls_version
   }
 }
--- variables.tf
+++ variables.tf
@@ -16,4 +16,35 @@ variable "appservice_name" {
 variable "web_app_name" {
   type        = string
   description = "name of the azurerm_linux_web_app"
+
+}
+
+variable "kind" {
+  type        = string
+  description = "The kind of the App Service Plan to create. Possible values are Windows (also available as App), Linux, elastic (for Premium Consumption) and FunctionApp (for a Consumption Plan). Defaults to Windows."
+  default     = "Linux"
+}
+
+variable "reserved" {
+  type        = bool
+  description = " Is this App Service Plan Reserved. Defaults to false."
+  default     = true
+}
+
+variable "sku_tier" {
+  type        = string
+  description = " Specifies the plan's pricing tier."
+  default     = "Standard"
+}
+
+variable "sku_size" {
+  type        = string
+  description = " Specifies the plan's instance size."
+  default     = "S1"
+}
+
+variable "minimum_tls_version" {
+  type        = string
+  description = "(optional) describe your variable"
+  default     = "1.2"
 }
