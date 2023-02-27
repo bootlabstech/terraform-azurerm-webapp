07ffb4e1ccc845b6eadd4eddf9042f9f1c921ea8
lalitha2407 <lalitha@bootlabstech.com>
2023-02-27 13:03:18 +0530

    removed app service block

--- output.tf
+++ output.tf
@@ -1,6 +1,6 @@
-output "azurerm_app_service_plan" {
-  value = azurerm_app_service_plan.appservice
-}
+# output "azurerm_app_service_plan" {
+#   value = azurerm_app_service_plan.appservice
+# }
 
 output "azurerm_linux_web_app" {
   value = azurerm_linux_web_app.web_app
