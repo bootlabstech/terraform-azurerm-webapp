4d759d119eb8599f2330144e8154a45c12364772
lalitha2407 <lalitha@bootlabstech.com>
2023-04-04 18:41:29 +0530

    ftps_state added

--- main.tf
+++ main.tf
@@ -20,5 +20,6 @@ resource "azurerm_linux_web_app" "web_app" {
   site_config {
     minimum_tls_version = var.minimum_tls_version
     app_command_line = var.app_command_line
+    ftps_state = "FtpsOnly"
   }
 }
